7cb0aa669000f49fe0ecd8bba7f6cb544c5e3dab
Francis Russell <francis@unchartedbackwaters.co.uk>
2022-11-15 16:02:49 +0000

    Add WIP implementation of process output listeners.

--- src/actions/exec.rs
+++ src/actions/exec.rs
@@ -1,5 +1,8 @@
+use crate::info;
 use crate::node::path::Path;
 use js_sys::JsString;
+use js_sys::Object;
+use wasm_bindgen::closure::Closure;
 use wasm_bindgen::JsValue;
 
 pub async fn exec<S: Into<JsString>, A: IntoIterator<Item = S>>(
@@ -8,7 +11,21 @@ pub async fn exec<S: Into<JsString>, A: IntoIterator<Item = S>>(
 ) -> Result<i32, JsValue> {
     let command_line: JsString = command_line.into();
     let args: Vec<JsString> = args.into_iter().map(|a| a.into()).collect();
-    ffi::exec(&command_line, Some(args), None)
+    let options = js_sys::Map::new();
+    let listeners = js_sys::Map::new();
+    let closure_o: Closure<dyn Fn(JsValue)> =
+        Closure::new(|value| info!("stdout got a value: {:?}", value));
+    let closure_e: Closure<dyn Fn(JsValue)> =
+        Closure::new(|value| info!("stderr got a value: {:?}", value));
+    listeners.set(&"stdline".into(), closure_o.as_ref());
+    listeners.set(&"errline".into(), closure_e.as_ref());
+    let listeners =
+        Object::from_entries(&listeners).expect("Failed to convert listeners map to object");
+    info!("Listeners set to: {:?}", listeners.to_string());
+    options.set(&"listeners".into(), &listeners);
+    let options = Object::from_entries(&options).expect("Failed to convert options map to object");
+    info!("Options set to: {:?}", options.to_string());
+    ffi::exec(&command_line, Some(args), &options)
         .await
         .map(|r| r.as_f64().expect("exec didn't return a number") as i32)
 }
@@ -19,13 +36,11 @@ pub mod ffi {
 
     #[wasm_bindgen(module = "@actions/exec")]
     extern "C" {
-        pub type ExecOptions;
-
         #[wasm_bindgen(catch)]
         pub async fn exec(
             comand_line: &JsString,
             args: Option<Vec<JsString>>,
-            options: Option<&ExecOptions>,
+            options: &JsValue,
         ) -> Result<JsValue, JsValue>;
     }
 }
