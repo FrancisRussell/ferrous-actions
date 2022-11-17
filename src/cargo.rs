8b668b2b269dcb06a1e7663421bb80a39872a57c
Francis Russell <francis@unchartedbackwaters.co.uk>
2022-11-17 22:50:42 +0000

    Intercept Cargo JSON output.

--- src/cargo.rs
+++ src/cargo.rs
@@ -1,5 +1,6 @@
-use crate::actions::exec::Command;
+use crate::actions::exec::{Command, Stdio};
 use crate::actions::io;
+use crate::info;
 use crate::node::path::Path;
 use crate::Error;
 
@@ -16,6 +17,10 @@ impl Cargo {
             .map_err(Error::Js)
     }
 
+    fn process_json_record(line: &str) {
+        info!("Got a JSON line: {}", line);
+    }
+
     pub async fn run<'a, I>(&'a mut self, subcommand: &'a str, args: I) -> Result<(), Error>
     where
         I: IntoIterator<Item = &'a str>,
@@ -23,14 +28,22 @@ impl Cargo {
         let subcommand = subcommand.to_string();
         let args: Vec<String> = args.into_iter().map(Into::into).collect();
         let mut final_args = Vec::new();
-        final_args.push(subcommand);
+        final_args.push(subcommand.clone());
+        let process_json = if subcommand == "clippy" {
+            final_args.push("--message-format=json".into());
+            true
+        } else {
+            false
+        };
         final_args.extend(args);
 
-        Command::from(&self.path)
-            .args(final_args)
-            .exec()
-            .await
-            .map_err(Error::Js)?;
+        let mut command = Command::from(&self.path);
+        command.args(final_args);
+        if process_json {
+            command.outline(|line| Self::process_json_record(line));
+            command.stdout(Stdio::null());
+        }
+        command.exec().await.map_err(Error::Js)?;
         Ok(())
     }
 }
