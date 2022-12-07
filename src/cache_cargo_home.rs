403115b3cbfa62c67a1ed509b46ede3e1b9ea69a
Francis Russell <francis@unchartedbackwaters.co.uk>
2022-12-07 17:49:20 +0000

    Try saving index to cache.

--- src/cache_cargo_home.rs
+++ src/cache_cargo_home.rs
@@ -1,10 +1,12 @@
+use crate::actions::cache::CacheEntry;
 use crate::fingerprinting::fingerprint_directory;
-use crate::info;
 use crate::node;
 use crate::node::os::homedir;
 use crate::node::path::Path;
 use crate::Error;
+use crate::{error, info};
 use serde::{Deserialize, Serialize};
+use std::borrow::Cow;
 
 fn find_cargo_home() -> Path {
     let mut path = homedir();
@@ -12,10 +14,9 @@ fn find_cargo_home() -> Path {
     path
 }
 
-fn find_index_path() -> Path {
+fn find_path(cache_type: CacheType) -> Path {
     let mut path = find_cargo_home();
-    path.push("registry");
-    path.push("index");
+    path.push(cache_type.relative_path());
     path
 }
 
@@ -24,12 +25,7 @@ fn cached_folder_info_path(cache_type: CacheType) -> Path {
     dir.push(".cache");
     dir.push("github-rust-actions");
     dir.push("cached_folder_info");
-    let file_name = {
-        let stem = match cache_type {
-            CacheType::Index => "index",
-        };
-        format!("{}.toml", stem)
-    };
+    let file_name = format!("{}.toml", cache_type.short_name());
     dir.push(file_name.as_str());
     dir
 }
@@ -37,6 +33,32 @@ fn cached_folder_info_path(cache_type: CacheType) -> Path {
 #[derive(Debug, Clone, Copy)]
 enum CacheType {
     Index,
+}
+
+impl CacheType {
+    fn short_name(&self) -> Cow<str> {
+        match *self {
+            CacheType::Index => "index",
+        }
+        .into()
+    }
+
+    fn friendly_name(&self) -> Cow<str> {
+        match *self {
+            CacheType::Index => "Registry index",
+        }
+        .into()
+    }
+
+    fn relative_path(&self) -> Path {
+        match *self {
+            CacheType::Index => {
+                let mut path = Path::from("registry");
+                path.push("index");
+                path
+            }
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -54,14 +76,33 @@ async fn build_cached_folder_info(path: &Path) -> Result<CachedFolderInfo, Error
     Ok(folder_info)
 }
 
+fn build_cache_entry(cache_type: CacheType, path: &Path) -> CacheEntry {
+    use crate::nonce::build_nonce;
+    let nonce = build_nonce(8);
+    let nonce = base64::encode_config(&nonce, base64::URL_SAFE);
+    let name = cache_type.friendly_name();
+
+    let primary_key = format!("{} - {}", name, nonce);
+    let mut cache_entry = CacheEntry::new(primary_key.as_str());
+    let secondary_key = format!("{}", name);
+    cache_entry.restore_key(secondary_key.as_str());
+    cache_entry.path(path);
+    cache_entry
+}
+
 pub async fn restore_cargo_cache() -> Result<(), Error> {
     // TODO: Delete index if it already exists (and probably warn)
     // TODO: Attempt index restore
     // TODO: If restore failed, make sure we have an empty folder
-    let index_path = find_index_path();
-    let folder_info = build_cached_folder_info(&index_path).await?;
+    let cache_type = CacheType::Index;
+    let folder_path = find_path(cache_type);
+    let folder_info = build_cached_folder_info(&folder_path).await?;
     let folder_info_serialized = serde_json::to_string_pretty(&folder_info)?;
-    info!("Index info: {}", folder_info_serialized);
+    info!(
+        "{} info: {}",
+        cache_type.friendly_name(),
+        folder_info_serialized
+    );
     let folder_info_path = cached_folder_info_path(CacheType::Index);
     let parent = folder_info_path.parent();
     node::fs::create_dir_all(&parent).await?;
@@ -72,10 +113,11 @@ pub async fn restore_cargo_cache() -> Result<(), Error> {
 pub async fn save_cargo_cache() -> Result<(), Error> {
     use wasm_bindgen::JsError;
 
-    let index_path = find_index_path();
-    let folder_info_new = build_cached_folder_info(&index_path).await?;
+    let cache_type = CacheType::Index;
+    let folder_path = find_path(cache_type);
+    let folder_info_new = build_cached_folder_info(&folder_path).await?;
     let folder_info_old: CachedFolderInfo = {
-        let folder_info_path = cached_folder_info_path(CacheType::Index);
+        let folder_info_path = cached_folder_info_path(cache_type);
         let folder_info_serialized = node::fs::read_file(&folder_info_path).await?;
         serde_json::de::from_slice(&folder_info_serialized)?
     };
@@ -87,12 +129,22 @@ pub async fn save_cargo_cache() -> Result<(), Error> {
         return Err(Error::Js(error.into()));
     }
     if folder_info_old.fingerprint == folder_info_new.fingerprint {
-        info!("{} unchanged, no need to write to cache", index_path);
+        info!("{} unchanged, no need to write to cache", folder_path);
     } else {
         info!(
             "{} fingerprint changed from {} to {}",
-            index_path, folder_info_old.fingerprint, folder_info_new.fingerprint
+            folder_path, folder_info_old.fingerprint, folder_info_new.fingerprint
         );
+        let cache_entry = build_cache_entry(CacheType::Index, &folder_path);
+        if let Err(e) = cache_entry.save().await.map_err(Error::Js) {
+            error!(
+                "Failed to save {} to cache: {}",
+                cache_type.friendly_name(),
+                e
+            );
+        } else {
+            info!("Saved {} to cache.", cache_type.friendly_name());
+        }
     }
     Ok(())
 }
