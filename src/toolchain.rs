61732a444e61df9ce51af92a801003eba73a4572
Francis Russell <francis@unchartedbackwaters.co.uk>
2022-12-10 16:45:27 +0000

    Don't try to remove existing cargo installs from the path.
    This causes issues if the current install path is in $CARGO_HOME
    and the user wants to install binaries there.

--- src/toolchain.rs
+++ src/toolchain.rs
@@ -152,81 +152,6 @@ async fn fetch_and_decompress_package(package: &ManifestPackage) -> Result<(), E
     Ok(())
 }
 
-async fn is_user_cargo_bin(folder: &Path) -> bool {
-    // The aim here is to remove all other cargo installs from the path without
-    // accidentally making the system unusable (e.g. removing /usr/bin)
-    let cargo_path = {
-        let mut cargo_path = folder.clone();
-        cargo_path.push("cargo");
-        cargo_path
-    };
-    if !cargo_path.exists().await {
-        return false;
-    }
-    let is_local = {
-        let folder = folder.to_string();
-        let home_dir = node::os::homedir().to_string();
-        folder.starts_with(&home_dir)
-    };
-    if !is_local {
-        return false;
-    }
-    true
-}
-
-pub async fn set_cargo_bin(cargo_bin: &Path) {
-    let environment = node::process::get_env();
-    let delimiter = node::path::delimiter();
-    let (path_len, paths): (_, Vec<_>) = {
-        let value = environment.get("PATH").map_or("", String::as_str);
-        let path_len = value.len();
-        (path_len, value.split(delimiter.as_str()).map(Path::from).collect())
-    };
-    let mut already_in_path = false;
-    let mut removed = Vec::new();
-    let mut paths: Vec<Path> = {
-        let mut result = Vec::with_capacity(paths.len());
-        for p in paths {
-            let keep = if p == *cargo_bin {
-                already_in_path = true;
-                true
-            } else if is_user_cargo_bin(&p).await {
-                removed.push(p.clone());
-                false
-            } else {
-                true
-            };
-            if keep {
-                result.push(p);
-            }
-        }
-        result
-    };
-    for path in &removed {
-        info!("Removed existing toolchain at {} from the path", path);
-    }
-    if already_in_path {
-        info!("Toolchain at {} was already in the path", cargo_bin);
-    } else {
-        info!("Adding {} to the path", cargo_bin);
-        paths.push(cargo_bin.clone());
-    }
-    let changed = !already_in_path || !removed.is_empty();
-    if changed {
-        let mut first = true;
-        let mut path_var = String::with_capacity(path_len);
-        for path in paths {
-            if first {
-                first = false;
-            } else {
-                path_var += delimiter.as_str();
-            }
-            path_var += path.to_string().as_str();
-        }
-        actions::core::export_variable("PATH", path_var);
-    }
-}
-
 pub async fn install_toolchain(toolchain_config: &ToolchainConfig) -> Result<(), Error> {
     use actions::tool_cache;
     use futures::{StreamExt as _, TryStreamExt as _};
@@ -269,7 +194,7 @@ pub async fn install_toolchain(toolchain_config: &ToolchainConfig) -> Result<(),
             cargo_bin.push("bin");
             cargo_bin
         };
-        set_cargo_bin(&cargo_bin).await;
+        actions::core::add_path(&cargo_bin);
     }
     Ok(())
 }
