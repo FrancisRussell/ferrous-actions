cf963f31b7052c433b4f36f2ba3b455d4f9e4df3
Francis Russell <francis@unchartedbackwaters.co.uk>
2022-12-18 15:23:06 +0000

    Rename `MathRandomRng`.

--- src/nonce.rs
+++ src/nonce.rs
@@ -1,9 +1,9 @@
-use crate::system::rng::MathRandomRng;
+use crate::system::rng;
 use rust_toolchain_manifest::HashValue;
 
 pub fn build(num_bytes: usize) -> HashValue {
     let mut bytes = vec![0u8; num_bytes];
-    let mut rng = MathRandomRng::default();
+    let mut rng = rng::MathRandom::default();
     rng.fill_bytes(&mut bytes);
     HashValue::from_bytes(&bytes)
 }
