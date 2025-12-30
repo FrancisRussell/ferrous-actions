9b6518f8df9ccc22512f9a282b2999ea2e88969e
Francis Russell <francis@unchartedbackwaters.co.uk>
2025-12-30 16:10:50 +0000

    Bump `getrandom`.

--- phase-wrappers/post.js
+++ phase-wrappers/post.js
@@ -1,7 +1,3 @@
-// See "Node.js ES module support" at https://docs.rs/getrandom/0.2.8/getrandom/
-const { webcrypto } = require('node:crypto')
-globalThis.crypto = webcrypto
-
 const { env } = require('node:process');
 env.GITHUB_RUST_ACTION_PHASE = 'post';
 const impl = require('./index.js');
--- src/nonce.rs
+++ src/nonce.rs
@@ -2,6 +2,6 @@ use rustup_toolchain_manifest::HashValue;
 
 pub fn build(num_bytes: usize) -> HashValue {
     let mut bytes = vec![0u8; num_bytes];
-    getrandom::getrandom(&mut bytes).unwrap_or_else(|e| panic!("Unable to get random data: {}", e));
+    getrandom::fill(&mut bytes).unwrap_or_else(|e| panic!("Unable to get random data: {}", e));
     HashValue::from_bytes(&bytes)
 }
